e3fb3648d3bb9d77f46b59d71af12bcd3356d5fa
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-12-27 17:24:39 -0300

    Fist commit

---
+++ five-app/src/main.ts
@@ -0,0 +1,19 @@
+
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import App from './App.vue'
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.my-app-dark',
+        }
+    }
+});
+
+app.mount('#app');
